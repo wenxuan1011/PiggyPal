6891da8eba6b9d0215a87cfc923ce208337aa248
yitengzhuang <yitengzhuang@merry>
2022-05-11 21:03:23 +0800

    record

--- parcel/test.js
+++
@@ -1,8 +0,0 @@
-import transmit from './signup.js' 
-
-$('#login button[type="submit"]').click((event)=> {
-    event.preventDefault()
-    setTimeout(()=>{
-        $('#login-output').html(transmit)
-    },500)
-})
