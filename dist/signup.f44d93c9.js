e8e27848c9c88ff85c092181b0721943bfe7e2d3
wenxuan1011 <wenxuan@merry>
2022-06-02 14:55:04 +0800

    project 60%

--- dist/signup.f44d93c9.js
+++ dist/signup.f44d93c9.js
@@ -1323,7 +1323,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "45144" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "45644" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
