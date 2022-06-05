b80815a4cda7cf73d5c5209d14c781c5d8febd0d
wenxuan1011 <wenxuan@merry>
2022-06-05 20:34:48 +0800

    save

--- dist/mainpage.c7d40710.js
+++ dist/mainpage.c7d40710.js
@@ -1501,7 +1501,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "43099" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "33860" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
